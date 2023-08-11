b026cd5b9bfae02cf7eb02dd4e525d8114bd832f
mateob24 <bernalmateoa@gmail.com>
2023-08-10 23:13:05 -0500

    Agregando evento y url para trailer

---
+++ url.js
@@ -0,0 +1,63 @@
+
+export const urlMovies = [
+    {
+        "titulo": "John Wick 1",
+        "trailerUrl": "./videos/johnWick1.mp4"
+    },
+    {
+        "titulo": "John Wick 2",
+        "trailerUrl": "./videos/jhonwick2.mp4"
+    },
+    {
+        "titulo": "John Wick 3",
+        "trailerUrl": "./videos/johnwick3.mp4"
+    },
+    {
+        "titulo": "John Wick 4",
+        "trailerUrl": "./videos/johnwick4.mp4"
+    },
+    {
+        "titulo": "Lucy",
+        "trailerUrl": "./videos/lucy.mp4"
+    },
+    {
+        "titulo": "Sin Limites",
+        "trailerUrl": "./videos/sinlimites.mp4"
+    },
+    {
+        "titulo": "Halloween 1",
+        "trailerUrl": "./videos/halloween1.mp4"
+    },
+    {
+        "titulo": "Halloween 2",
+        "trailerUrl": "./videos/halloween2.mp4"
+    },
+    {
+        "titulo": "Halloween 3",
+        "trailerUrl": "./videos/halloween3.mp4"
+    },
+    {
+        "titulo": "Halloween 4",
+        "trailerUrl": "./videos/halloween4.mp4"
+    },
+    {
+        "titulo": "Halloween 5",
+        "trailerUrl": "./videos/halloween5.mp4"
+    },
+    {
+        "titulo": "Halloween 6",
+        "trailerUrl": "./videos/halloween6.mp4"
+    },
+    {
+        "titulo": "Halloween 7",
+        "trailerUrl": "./videos/halloween7.mp4"
+    },
+    {
+        "titulo": "Halloween 8",
+        "trailerUrl": "./videos/halloween8.mp4"
+    },
+    {
+        "titulo": "Halloween El Final", 
+        "trailerUrl": "https://www.youtube.com/watch?v=F90MmnTnSUQ&ab_channel=UniversalSpain"
+    }
+]
