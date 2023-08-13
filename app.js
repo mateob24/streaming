1304e9b3da8dcbad0a811e7dd49f90ea8b90e26b
mateob24 <bernalmateoa@gmail.com>
2023-08-13 14:26:45 -0500

    Add new files and little changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import { urlMovies } from "./url.js";
 import { urlSeries } from "./url.js";
 
+//Elementos HTML con sus eventos para mostrar un trailer a través del 'btn'
 const trailer = document.getElementById('trailer')
 const modal = document.getElementById('videoModal');
 const closeModal = modal.querySelector('.close')
@@ -34,36 +35,45 @@ const stopVideo = () => {
 };
 
 
-const searchInput = document.getElementById("searchInput");
-const movieSerieCards = document.querySelectorAll(".movie-card, .serie-card");
+
+//Filtrado a través de una búsqueda del titulo de la película
+const searchInput = document.getElementById("search-input");
+const movieCards = document.querySelectorAll(".movie-card");
+const serieCards = document.querySelectorAll('.serie-card')
+const movieTitle = document.querySelector(".title-movies");
+const serieTitle = document.querySelector(".title-series");
 
 searchInput.addEventListener("input", function() {
     const searchTerm = searchInput.value.toLowerCase();
+    //Se necesita para saber si en el resultado de la búsqueda hay o no hay películas o series
+    let hasMovieResults = false;
+    let hasSerieResults = false;
 
-    movieSerieCards.forEach(card => {
+    movieCards.forEach(card => {
         const title = card.querySelector(".titulo").textContent.toLowerCase();
         if (title.includes(searchTerm)) {
             card.style.display = "flex";
+            hasMovieResults = true
         } else {
             card.style.display = "none";
         }
     });
+
+    serieCards.forEach(card => {
+      const title = card.querySelector(".titulo").textContent.toLowerCase();
+      if (title.includes(searchTerm)) {
+          card.style.display = "flex";
+          hasSerieResults = true;
+      } else {
+          card.style.display = "none";
+      }
+  }); 
+
+  //Lo usamos para hacer un tipo de condicional que nos permitirá ocultar el titulo de series
+  //o películas en caso de que la búsqueda no arroje alguna de estas dos
+  movieTitle.style.display = hasMovieResults ? "flex" : "none";
+  serieTitle.style.display = hasSerieResults ? "flex" : "none";
 });
-
-
-
-
-
-
-// const btnMovie = document.getElementById('movies')
-
-// btnMovie.addEventListener('click', () => {
-//     const hideSection = document.querySelector('.serie-container')
-//     if (hideSection.style.display === "flex") {
-//         hideSection.style.display = "none"
-//     }
-// })
-
 
 
 
@@ -80,16 +90,16 @@ function filtrarPorCategoria(categoria) {
         card.style.display = 'none';
       }
     });
+  
   }
 
-  const titleMovies = document.querySelector('.title-movies')
-  const titleSeries = document.querySelector('.title-series')
   document.getElementById('categoria-select').addEventListener('change', function() {
     const seleccionada = this.value;
     filtrarPorCategoria(seleccionada);
-    if (titleSeries.style.display=="flex") {
-      
-    }
-    titleSeries.style.display="none"
   });
+  
+
+
+
+
   
